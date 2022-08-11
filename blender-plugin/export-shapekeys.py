4ae08909065ede17277247a5c04e2a6f4e34c182
Yū Kobayashi <kb10uy@kb10uy.org>
2022-08-11 17:59:07 +0900

    Update blender plugin, now emits TOML

--- blender-plugin/export-shapekeys.py
+++ blender-plugin/export-shapekeys.py
@@ -5,7 +5,7 @@ import json
 bl_info = {
     "name": "Shape Key Exporter",
     "author": "kb10uy",
-    "version": (1, 0),
+    "version": (2, 0),
     "blender": (3, 2, 0),
     "location": "",
     "description": "",
@@ -16,10 +16,15 @@ bl_info = {
 class ShapeKeyExporter(bpy.types.Operator):
     bl_idname = "kb10uy.shapekeyexporter_export"
     bl_label = "Export Shape Keys"
-    bl_description = "Export shape keys as JSON"
+    bl_description = "Export shape keys as TOML"
     bl_options = {"REGISTER"}
 
     def execute(self, context: Any) -> set[str]:
+        toml_string = ""
+
+        toml_string += "name = \"AvatarName\"\n"
+        toml_string += "\n"
+
         animation_objects = []
         for selected_object in bpy.context.selected_objects:
             if selected_object.type != "MESH":
@@ -31,14 +36,12 @@ class ShapeKeyExporter(bpy.types.Operator):
             mesh_object: bpy.types.Mesh = selected_object.data
             shape_key_blocks = mesh_object.shape_keys.key_blocks
 
-            animation_object = {
-                "name": selected_object.name,
-                "groups": [],
-            }
+            toml_string += "[[mesh_groups]]\n"
+            toml_string += "mesh = \"Ungrouped\"\n"
+            toml_string += f"mesh = \"{selected_object.name}\"\n"
 
-            shapes = []
+            toml_string += "options = [\n"
             basis_skipped = False
-            parameter_index = 1
             for name, shape_key in shape_key_blocks.items():
                 if not basis_skipped:
                     basis_skipped = True
@@ -49,40 +52,12 @@ class ShapeKeyExporter(bpy.types.Operator):
                         f"{name} moves no vertex, skipping..."
                     )
                     continue
+                toml_string += f"  {{ label = \"{name}\", shapes = [\"{name}\"] }},\n"
 
-                shapes.append({
-                    "animation_name": name,
-                    "shape_name": name,
-                    "index": parameter_index,
-                })
-                parameter_index += 1
+            toml_string += "]\n"
+            toml_string += "\n"
 
-            unset_group = {
-                "name": "Unset",
-                "emit": False,
-                "animation_type": "select",
-                "shapes": shapes,
-            }
-            animation_object["groups"] = [unset_group]
-            animation_objects.append(animation_object)
-
-            self.report(
-                {"DEBUG"},
-                f"Exported {len(shapes)} key(s) for {mesh_object.name}"
-            )
-
-        animation_descriptor = {
-            "name": "AvatarName",
-            "animation_objects": animation_objects,
-        }
-
-        json_string = json.dumps(
-            animation_descriptor,
-            ensure_ascii=False,
-            indent=4
-        )
-
-        self.report({"INFO"}, json_string)
+        self.report({"INFO"}, toml_string)
         return {"FINISHED"}
 
 
